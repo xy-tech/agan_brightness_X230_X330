0ad3b6f23c588c2d5630aeaed3e4344b5318559d
Xue Yao <xue.yao.n@gmail.com>
2020-12-28 17:12:33 +0800

    updated script

--- script.c
+++ script.c
@@ -1,8 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 #include "hidapi/hidapi.h"
 
-int main (void)
+int main (int argc, char *argv[])
 {
+	if (argc > 1)
+	{
+		printf("too many arguments");
+	}
+
 	int res;
 	hid_device *handle;
 	
@@ -19,15 +26,28 @@ int main (void)
 		printf("device open!");
 	}
 	
-
 		
 	//write to 0x06, followed by the value of brightness
 	//read from 0x04, followed by the actual value of brightness
 	//value to write is in multiples of 16
-	//value to read is just 0 to 15	
-	const unsigned char buf[2] = {6, 240}; 
-
+	//value to read is just 0 to 15
+	
 	int buflen = 2;
+	unsigned char buf[2];
+	buf[0] = 6;
+
+	if (argc == 0)
+	{	
+		buf[1] = 240;
+		printf("max brightness");
+	}
+	else
+	{
+		buf[1] = atoi(argv[1]) * 16;
+		printf("adjust brightness");
+	}
 
 	res = hid_write(handle, buf, buflen);
+
+	return 0;
 }
